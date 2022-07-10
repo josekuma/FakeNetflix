d1e4b7cfd7d9ec177753c72121f0fc6965b8fe21
josecgj <josekgj@gmail.com>
2022-07-10 13:49:00 +0200

    Home and sign pages made but with no functionality

--- src/Requests.js
+++ src/Requests.js
@@ -1,8 +1,9 @@
 const key= '1d69909edb3ac66c82b14d120e6a1122'
 
 const requests={
-    requestLatest: `https://api.themoviedb.org/3/movie/latest?api_key=${key}&language=en-US`,
-    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestUpComing: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`
 }
 
 export default requests;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between items-center p-4 z-[100] w-full absolute'>
-        <h1 className='text-red-600 text-4xl'>NETPLIX</h1>
-        <div>
-            <button className='text-white pr-4'>Sign In</button>
-            <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Sign Up</button>
-        </div>
+    <div className="flex justify-between items-center p-4 z-[100] w-full absolute">
+      <Link to="/">
+        <h1 className="text-red-600 text-4xl">NETPLIX</h1>
+      </Link>
+      <div>
+        <Link to="/login">
+          <button className="text-white pr-4">Sign In</button>
+        </Link>
+        <Link to="/signup">
+          <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+            Sign Up
+          </button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
