0ac7f475f7e727145019d4cdee909117daa076de
josecgj <josekgj@gmail.com>
2022-07-10 19:31:54 +0200

    Finished account page and add favorites function

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,23 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 const Login = () => {
-  const { user } = UserAuth();
+  const { user, logIn } = UserAuth();
+  const [error, setError] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await logIn(email, password);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+    }
+  };
 
   return (
     <>
@@ -12,13 +27,20 @@ const Login = () => {
           <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
             <div className="max-w-[320px] mx-auto py-16">
               <h1 className="text-3xl font-bold">Sign In</h1>
-              <form className="w-full flex flex-col py-4">
+              {error ? <p className="p-3 bg-red-400 my-2">{error}</p> : null}
+              <form
+                onSubmit={handleSubmit}
+                className="w-full flex flex-col py-4"
+              >
                 <input
+                  onChange={(e) => setEmail(e.target.value)}
                   className="p-3 my-2 rounded"
                   type="email"
                   placeholder="Email"
+                  autoComplete="email"
                 />
                 <input
+                  onChange={(e) => setPassword(e.target.value)}
                   className="p-3 my-2 rounded"
                   type="password"
                   placeholder="Password"
@@ -33,7 +55,7 @@ const Login = () => {
                   <p>Need Help?</p>
                 </div>
                 <p className="py-8">
-                  <span className="text-gray-600">New to to Netflix?</span>{" "}
+                  <span className="text-gray-600">New to to Netplix?</span>{" "}
                   <Link to="/signup">Sign Up</Link>
                 </p>
               </form>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 const Signup = () => {
+  const { user, signUp } = UserAuth();
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signUp(email, password);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <div className="w-full h-screen bg-slate-400">
@@ -9,16 +24,23 @@ const Signup = () => {
           <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
             <div className="max-w-[320px] mx-auto py-16">
               <h1 className="text-3xl font-bold">Sign Up</h1>
-              <form className="w-full flex flex-col py-4">
+              <form
+                onSubmit={handleSubmit}
+                className="w-full flex flex-col py-4"
+              >
                 <input
+                  onChange={(e) => setEmail(e.target.value)}
                   className="p-3 my-2 rounded"
                   type="email"
                   placeholder="Email"
+                  autoComplete="email"
                 />
                 <input
+                  onChange={(e) => setPassword(e.target.value)}
                   className="p-3 my-2 rounded"
                   type="password"
                   placeholder="Password"
+                  autoComplete="current-password"
                 />
                 <button className="bg-red-600 py-3 my-6 rounded font-bold">
                   Sign Up
@@ -31,7 +53,7 @@ const Signup = () => {
                 </div>
                 <p className="py-8">
                   <span className="text-gray-600">
-                    Already subscribed to Netflix?
+                    Already subscribed to Netplix?
                   </span>{" "}
                   <Link to="/login">Sign In</Link>
                 </p>
